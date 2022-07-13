5affc3a598e74fef5da5ca41ba8d7f8bbe3512d6
Raj Kumar Singh <66634357+raj-26singh@users.noreply.github.com>
2022-07-13 21:02:26 +0530

    Updated 13 July at 21:02

--- water_flooding.py
+++ water_flooding.py
@@ -1,10 +1,12 @@
 
+from numpy.core.defchararray import count
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import numpy as np
 import streamlit as st
 import warnings
 warnings.filterwarnings("ignore")
+import pandas as pd
 
 def water_flooding():
     
@@ -28,19 +30,54 @@ def water_flooding():
     
     
     # Assumed Parameters
+    
     Sw = (np.linspace(100,850,num=751))/1000.0
+    Sw = np.flipud(Sw)
     
     # Calculations
 
     # Relative Permeabilities
+    
     kro = Krocw*(((1.0-Sw-Sorw)/(1.0-Swc-Sorw))**No)
     krw = Krwiro*(((Sw-Swc)/(1.0-Swc-Sorw))**Nw)
 
+    # Plotting the relative permeability curves
+    
+    fig_perm = make_subplots(specs=[[{"secondary_y": True}]])
+    fig_perm.add_trace(go.Line(x=np.insert(np.append(Sw,0),0,1.0), y=np.insert(np.append(kro,kro[-1]),0,0.0),name="Relative Permeability of Oil (kro)"),secondary_y=False,)
+    fig_perm.add_trace(go.Line(x=np.insert(np.append(Sw,0),0,1.0), y=np.insert(np.append(krw,0.0),0,krw[0]),name="Relative Permeability of Water (krw)"),secondary_y=True,)
+    fig_perm.update_yaxes(title_text="<b>Relative Permeability of Oil (kro)</b>", secondary_y=False, range=[0.0,1.0],nticks=20)
+    fig_perm.update_yaxes(title_text="<b>Relative Permeability of Water (krw)</b>", secondary_y=True, range=[0.0,1.0],nticks=20)
+    fig_perm.update_xaxes(title_text="<b>Water Saturation (Sw)</b>",range=[0.0,1.0],nticks=20)
+    fig_perm.update_layout(title={'text':"<b>Relative Permeability Curves<b>",'x':0.48,'y':0.85},legend=dict(x=0.25,y=-0.39))
+    st.plotly_chart(fig_perm, use_container_width=True)
+    
+
     # Mobility Ratio
+    
     M = (krw/uw)/(kro/uo)
 
+
     # Fractional flow of water
+    
     fw = 1.0/(1.0+(1.0/M))
+    
+    # Plotting the fractional flow curve
+    
+    pos = 0
+    for f in range(len(fw)):
+        if fw[f]==1.0:
+            pos = f
+            break
+            
+    st.write(pos)
+    fig_fw = make_subplots()
+    fig_fw.add_trace(go.Line(x=Sw[pos:], y=fw[pos:],name="Fractional Flow Curve"))
+    fig_fw.update_yaxes(title_text="<b>Fractional Flow of water (fw)</b>",range=[0.0,1.0],nticks=20)
+    fig_fw.update_xaxes(title_text="<b>Water Saturation (Sw)</b>",range=[0.0,1.0],nticks=20)
+    fig_fw.update_layout(title={'text':"<b>Fractional Flow Curve<b>",'x':0.48,'y':0.85})
+    st.plotly_chart(fig_fw, use_container_width=True)
+    
 
     delfw_delSw = (fw-0.0)/(Sw-Swc)
 
@@ -48,71 +85,37 @@ def water_flooding():
 
     diff = abs(dfw_dSw-delfw_delSw)
 
-    min_index = 101 + np.argmin(diff[101:])
-    
+    min_index = 5 + np.argmin(diff[5:(len(Sw)-101)])
     SwBT = Sw[min_index]
     
 
     t_BT = (phi*area*L)/(qt*dfw_dSw[min_index])
     
+    
+    # Taking input of Time
+    
     st.write('\nEnter the time (in days) for which waterflooding has been done.')
     time = st.slider('Time (in days)', min_value=0, max_value=round(t_BT)+1, value=100)
     
-    #def get_values(time):
     xD = (qt*time/(phi*area*L))*dfw_dSw
-    #x_bt = (qt*time/(phi*area*L))*(dfw_dSw[min_index])
-    max_ind = 101 + np.argmax(xD[101:])
-    #xdm = 100*round(xD[max_ind])
-    #xD = np.append(xD, (np.repeat(xdm,100))/100.0)
-    #return xD, max_index
+    max_ind = 5 + np.argmax(xD[5:(len(Sw)-101)])
+    
+    #st.write("Max ind:",max_ind,"Sw[max_ind]:",Sw[max_ind],"xD[max_ind]",xD[max_ind])
 
-    for i in range(len(Sw)):
-        if Sw[i]<Sw[max_ind]:
-            Sw[i] = Swc
-            xD[i] = 1 - xD[i]
-            if xD[i]<xD[max_ind]:
-                xD[i] = xD[max_ind]
-            
-               
+    # Making Adjustments for Plot
+    xD[max_ind:] = (np.linspace((round(100*(xD[max_ind]-0.0025))),100,num=(len(xD)-max_ind)))/100.0
+    Sw[max_ind:] = np.repeat(Swc,(len(xD)-max_ind))
     
-    #xD, max_ind = get_values(time)
-    
-    # SwBT ka jugaad
-    '''
-    jugaad_Sw = np.zeros(len(Sw))
-    count = 0
-    key = 0
-    for i in range(len(Sw)):
-        if Sw[i]>=Sw[max_ind]:
-            jugaad_Sw[i] = Sw[i]
-        else:
-            jugaad_Sw[i] = Swc
-            if key==0:
-                pos = i
-                key = 1
-            count = count + 1
-        
-    
-    #xdm = 100*round(xD[max_ind])
-    x_swc = (np.linspace(xdm,100,num=count))/100.0
-    for i in range(count):
-        xD[pos+i] = x_swc[i]
-    
-    #xD[pos:] = x_swc
-
-    a = 100*round(Swc)
-    b = 100*round(Sw[max_ind])
-    #jugaad_Sw = np.append(jugaad_Sw,(np.linspace(a,b,num=100)/100))
-    '''
+    #st.write(pd.DataFrame({"xD:": xD,"Sw:":Sw}))
     
     
-    
-    # Plotting
+    # Plotting the Saturation profile
     
     fig = make_subplots()
-    fig.add_trace(go.Line(x=xD, y=Sw,name="Saturation"))
+    fig.add_trace(go.Line(x=xD[1:], y=Sw[1:],name="Saturation"))
     fig.update_yaxes(title_text="<b>Saturation</b>",range=[0.0,1.0],nticks=20)
     fig.update_xaxes(title_text="<b>Dimensionless Distance (xD)</b>",range=[0.0,1.0],nticks=20)
+    fig.update_layout(title={'text':"<b>Saturation Profile<b>",'x':0.48,'y':0.85})
     st.plotly_chart(fig, use_container_width=True)
     
     
